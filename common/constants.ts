d7b4231b388658ca266a8ff2a3325993d020b48b
hoangngochanh <ngochanh.hoang@aia.com>
2022-11-16 15:59:56 +0700

    Connect metamask

---
+++ common/constants.ts
@@ -0,0 +1,22 @@
+const chainList = {
+  1: {
+    chainId: 1,
+    chainName: "Ethereum Mainnet",
+    rpc: "https://mainnet.infura.io/v3/",
+    explorer: "https://etherscan.io",
+  },
+  3: {
+    chainId: 3,
+    chainName: "Ropsten",
+    rpc: "https://rpc.ankr.com/eth_ropsten",
+    explorer: "https://ropsten.etherscan.io",
+  },
+  4: {
+    chainId: 4,
+    chainName: "Rinkeby",
+    rpc: "  https://rpc.ankr.com/eth_rinkeby",
+    explorer: "https://rinkeby.etherscan.io",
+  },
+};
+
+export default chainList;
---
+++ libs/web3.ts
@@ -0,0 +1,77 @@
+import Web3 from "web3";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+export const getProvider = () => {
+  if (typeof window !== "undefined") {
+    const { ethereum } = window as any;
+    return ethereum;
+  }
+};
+
+export const alreadyConnected = (): Promise<string> => {
+  const provider = getProvider();
+  if (provider != null) {
+    const web3 = new Web3(provider);
+
+    return web3.eth
+      .getAccounts()
+      .then((accounts) => {
+        return Promise.resolve(accounts[0]);
+      })
+      .catch((err) => {
+        return Promise.reject(err);
+      });
+  } else {
+    return Promise.reject("Please install metamask");
+  }
+};
+
+export const connectMetamask = (): Promise<string> => {
+  const provider = getProvider();
+  if (provider != null) {
+    const web3 = new Web3(provider);
+
+    return web3.eth
+      .requestAccounts()
+      .then((accounts) => {
+        return Promise.resolve(accounts[0]);
+      })
+      .catch((err) => {
+        return Promise.reject(err);
+      });
+  } else {
+    return Promise.reject("Please install metamask");
+  }
+};
+
+export const onAccountChange = (callback: (address: string | null) => void) => {
+  const provider = getProvider();
+  if (provider) {
+    provider.on("accountsChanged", function (accounts: string[]) {
+      if (accounts && accounts.length > 0) {
+        callback(Web3.utils.toChecksumAddress(accounts[0]));
+      } else {
+        callback(null);
+      }
+    });
+  }
+};
+
+export const onChainIdChange = (callback: (chainId: number) => void) => {
+  const provider = getProvider();
+  if (provider) {
+    provider.on("chainChanged", function (chainId: number) {
+      callback(Number(chainId));
+    });
+  }
+};
+
+export const getChainId = () => {
+  const web3 = new Web3(getProvider());
+  return web3.eth.getChainId().then((chainId) => Number(chainId));
+};
